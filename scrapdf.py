7d14a16436bf13a60e85c6296b07e0963852c721
David <cincoisc@gmail.com>
2024-11-26 11:35:36 -0700

    changes

---
+++ scrapdf.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import requests, bs4
+from decimal import Decimal, ROUND_DOWN
+from bookdb import BookDB
+
+website_url = "http://books.toscrape.com/"
+
+def extract_books(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        try:
+            return response
+        except Exception as e:
+            print(f"Status 400 in extract_books: {e}")
+            return None
+    else:
+        print(f"Status 400 in extract_books: {e}")
+        return None
+
+def transform_books(datas):
+    soup = bs4.BeautifulSoup(datas.text, "lxml")
+    books = soup.select(".product_pod")
+
+    collected_books = pd.DataFrame(
+        {
+            "TITLE": [],
+            "RATING": [],
+            "PRICE": [],
+            "STOCK": [],
+        }
+    )
+    
+    for book in books:
+        #Book's Title 
+        title = book.h3.a['title']        
+        #Book's Rating
+        if book.select_one(".One"):
+            rate = 1
+        elif book.select_one(".Two"):
+            rate = 2
+        elif book.select_one(".Three"):
+            rate = 3
+        elif book.select_one(".Four"):
+            rate = 4
+        elif book.select_one(".Five"):
+            rate = 5
+        else:
+            rate = None
+
+        #Price
+        data_price = book.select('.price_color')[0].getText()
+        price_split = data_price.split("Â")
+        price = Decimal(price_split[1].split("£")[1]).quantize(Decimal("0.00"), rounding=ROUND_DOWN)
+                
+        #Stock Available
+        #strip method to remove start and end whitespace
+        stock_available = book.select('.instock')[0].getText().strip()
+        if stock_available == "In stock":
+            stock = True
+        else:
+            stock = False
+        
+        # book_to_store = {
+        #  "title": title, "rating": rate,
+        #  "price": price, "stock": stock 
+        # }
+        
+        # collected_books.append(book_to_store)
+        collected_books.insert(title, rate, price, stock)
+
+    return collected_books
+
+# def load_books(books):
+
+extract = extract_books(website_url)
+if extract is not None:
+    transform = transform_books(extract)
+    print(transform)
